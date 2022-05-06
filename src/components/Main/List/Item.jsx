0ad88a30f905ae5dbe7d535615cc05a84076b31b
Manav Misra <manavm@visionwebsoft.com>
2022-05-06 11:44:28 -0500

    conditional render based on completed property

--- src/components/Main/List/Item.jsx
+++ src/components/Main/List/Item.jsx
@@ -1,7 +1,12 @@
 import Todo from "@/types/Todo";
 
 export default function Item({ todo }) {
-  return <li>{todo.text}</li>;
+  return (
+    <li className={todo.completed ? "completed" : null}>
+      {todo.text}
+      <input type="checkbox" checked={todo.completed} />
+    </li>
+  );
 }
 
 Item.propTypes = {
